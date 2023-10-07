787f31f94dff8bd302845ff196abf9d0c0b8acfd
Ronfos <74715764+Ronfos@users.noreply.github.com>
2023-10-07 18:49:13 +0300

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите пять слов:");
+            string a1 = Convert.ToString(Console.ReadLine());
+            string a2 = Convert.ToString(Console.ReadLine());
+            string a3 = Convert.ToString(Console.ReadLine());
+            string a4 = Convert.ToString(Console.ReadLine());
+            string a5 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine($"\n{a1}");
+            Console.WriteLine(a2);
+            Console.WriteLine(a3);
+            Console.WriteLine(a4);
+            Console.WriteLine(a5);
+            Console.ReadKey();
+        }
+    }
+}
